9d1892d1fcdd9ce464b489d2cf213365059533f2
Moxieq <dimanavsisto@gmail.com>
2019-11-04 00:13:49 +0200

    Changen structure of parsed messages. Typo fixed.

--- readEmail.py
+++ readEmail.py
@@ -4,15 +4,13 @@ from httplib2 import Http
 from oauth2client import file, client, tools
 import json
 import base64
+from functools import reduce
 
 SCOPES = 'https://www.googleapis.com/auth/gmail.readonly'
 
 def save_json(file_name, obj):
     with open(file_name+".json", "w") as file:
         json.dump(obj, file, indent=4, sort_keys=True)
-    with open(file_name+".txt", "w") as file:
-        file.write(obj[0][0]['message_body'])
-
 
 
 def get_attachment_by_id(service, user_id, msg_id, att_id):
@@ -24,31 +22,30 @@ def save_attachment(path, data):
         f.write(data)
 
 # proces the parts
-def parce_payload(msg_payload):
+def parse_payload(msg_payload):
     # chek if it has nested parts by checking mimeType
     if msg_payload["mimeType"][:9] == "multipart":
-        return [parce_payload(part) for part in msg_payload["parts"]]
+        # at first func parse_payload() executes for every part, then they merge into one dict by reduce()
+        return reduce(lambda p, nxt_p: {**p, **nxt_p}, map(parse_payload,msg_payload["parts"]))
     elif "body" in msg_payload:
         # check if there is any attachment
         if msg_payload["filename"]:
             if "data" in msg_payload["body"]:
                 data = msg_payload["body"]["data"]
-                return {"data":data}
+                return {msg_payload["filename"]:{"data":data}}
             else:
                 attachment_id = msg_payload["body"]["attachmentId"]
-                return {"file":
-                    {"attachment_id":attachment_id,
-                    "filename":msg_payload["filename"]}}
-        # elif msg_payload['mimeType'] == 'text/plain':
+                return {msg_payload["filename"]:
+                    {"attachment_id":attachment_id}}
         elif msg_payload['mimeType'][:4] == 'text':
             # make smth with parts like 0.1 0.2 (same body in html/text and plain/text)
             body_decoded = base64.urlsafe_b64decode(msg_payload['body']['data']).decode('UTF-8')
-            return {"message_body":body_decoded}
+            return {msg_payload['mimeType'][5:]:body_decoded}
     return None
 
 
 def parse_msg_obj(msg_obj):
-    return parce_payload(msg_obj["payload"])
+    return parse_payload(msg_obj["payload"])
 
 
 def main():
@@ -67,19 +64,20 @@ def main():
     service = build('gmail', 'v1', http=http)
     
     # Call the Gmail API to fetch INBOX
-    n_msg_to_show = 3;
+    n_msg_to_show = 1;
     results = service.users().messages().list(userId='me', 
             labelIds=['INBOX'], 
             includeSpamTrash=True, 
             maxResults=n_msg_to_show).execute()
 
-    messages_as_objects = [service.users().messages().get(userId='me', 
+    # get whole messages from gmail
+    msgs_objects = [service.users().messages().get(userId='me', 
         id=message['id']).execute() for message in results.get('messages', [])]
-    if not messages_as_objects:
+    if not msgs_objects:
         print("No messages found.")
     else:
-        messages = [parse_msg_obj(msg) for msg in messages_as_objects]
-        save_json("last_message", messages[2])
+        msgs = [parse_msg_obj(msg) for msg in msgs_objects]
+        save_json("last_message", msgs[0])
 
 if __name__ == '__main__':
     main()
