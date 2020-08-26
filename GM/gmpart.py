fb752aa85ce13ec73d69013eae0125d45f1add05
Moxxiq <dimanavsisto@gmail.com>
2020-08-26 19:33:50 +0300

    Read FULL messages

--- GM/gmpart.py
+++ GM/gmpart.py
@@ -4,7 +4,8 @@ from aiogoogle.auth.utils import create_secret
 
 
 class Gmpart():
-    # TODO: save user_creds in destructor
+    # TODO: save user_creds on exit from with
+    # : try wrap all request to `async with as` in decorator
     def __init__(self, CLIENT_CREDS, user_creds = None):
         """Init class with basic parameters
         Parameters:
@@ -18,31 +19,63 @@ class Gmpart():
         """
         self.CLIENT_CREDS = CLIENT_CREDS
         self.user_creds = user_creds
-        self.aiogoogle = None
         self.state = create_secret()
-        self.init_entry_point(self.user_creds)
 
-    def authorize_uri(self, EMAIL):
-        if self.aiogoogle.oauth2.is_ready(self.CLIENT_CREDS):
-            uri = self.aiogoogle.oauth2.authorization_url(
+    def authorize_uri(self, email):
+        """ Generate authozisation uri via aiogoogle's oauth wrapper
+        Parameters:
+        email (str): IDK some login_hint
+        """
+        aiogoogle = Aiogoogle(client_creds = self.CLIENT_CREDS)
+        if aiogoogle.oauth2.is_ready(self.CLIENT_CREDS):
+            uri = aiogoogle.oauth2.authorization_url(
                 client_creds=self.CLIENT_CREDS,
                 state=self.state,
                 access_type='offline',
                 include_granted_scopes=True,
-                login_hint=EMAIL,
+                login_hint=email,
                 prompt='select_account',
             )
         else:
             raise ServerError("Client doesn't have enough info for Oauth2")
         return uri
 
-    def init_entry_point(self, user_creds):
-        self.user_creds = user_creds
+    async def build_user_creds(self, code):
+        async with Aiogoogle(client_creds = self.CLIENT_CREDS) as aiogoogle:
+            self.user_creds = await aiogoogle.oauth2.build_user_creds(
+                grant = code,
+                client_creds = self.CLIENT_CREDS
+            )
+
+    def update_access_token(self, user_creds):
+        self.user_creds['access_token'] = user_creds['access_token']
+        self.user_creds['expires_in'] = user_creds['expires_in']
+        self.user_creds['expires_at'] = user_creds['expires_at']
+
         # remember, that aiogoogle has it's own Auth manager
         # so you don't need to refresh tocken by hand
-        self.aiogoogle = Aiogoogle(user_creds=self.user_creds, client_creds=self.CLIENT_CREDS)
 
     async def create_api(self):
-        async with self.aiogoogle as aiogoogle:
+        async with Aiogoogle(client_creds = self.CLIENT_CREDS) as aiogoogle:
             # Downloads the API specs and creates an API object
             return await aiogoogle.discover('gmail', 'v1')
+
+    async def messages_list(self, messages_num = 5):
+        async with Aiogoogle(client_creds = self.CLIENT_CREDS, user_creds = self.user_creds) as aiogoogle:
+            gmpart_api = await self.create_api()
+            messages_ids = await aiogoogle.as_user(
+                gmpart_api.users.messages.list(
+                    userId='me', 
+                    labelIds='INBOX', 
+                    includeSpamTrash=True, 
+                    maxResults=messages_num)
+                )
+            messages = await aiogoogle.as_user(
+                *tuple(gmpart_api.users.messages.
+                    get(userId='me', id=message['id']) 
+                    for message in 
+                    messages_ids['messages'])
+                )
+            self.update_access_token(aiogoogle.user_creds)
+        return messages
+
--- main.py
+++ main.py
@@ -1,54 +1,17 @@
-import sys, os, json
-import asyncio
+import sys, os, json, asyncio
 from GM import Gmpart
 from sanic import Sanic, response
 from sanic.exceptions import ServerError
-from config import TEST_CLIENT_CREDS, TEST_EMAIL
+from config import *
 
 from aiogoogle import Aiogoogle
 
-test_user_creds = {"access_token":"ya29.a0AfH6SMDgfx7hu0tMtbSBIYr92yqp4AWVSwa9TEhDM-81ier34dYnd3gFC0l_ihYbAAdK1UM6zq9Ibp2RyNAJxr2ZJk6ebzGPspMiWokT-BtBegWblrayZIUKi3JoEsOxRXzLuweVkjxO3KzyzdhQuBovJfjTCBaLhBg","refresh_token":"1//0cu1M5II2GlC_CgYIARAAGAwSNwF-L9IrfKciXIEawE8-fQN7nEZCKog4mQHl2XM2sooHwbmMsvYNrkTttDjVWIc5KN9AbVi4mHI","expires_in":3599,"expires_at":"2020-08-25T16:21:24.080087","scopes":["https://www.googleapis.com/auth/gmail.readonly"],"id_token":"null","id_token_jwt":"null","token_type":"Bearer","token_uri":"https://oauth2.googleapis.com/token","token_info_uri":"https://www.googleapis.com/oauth2/v4/tokeninfo","revoke_uri":"https://oauth2.googleapis.com/revoke"}
 
 LOCAL_ADDRESS = "localhost"
 LOCAL_PORT = "5000"
 
 app = Sanic(__name__)
-        
-#----------------------------------------#
-#                                        #
-# **Step A (Check OAuth2 figure above)** #
-#                                        #
-#----------------------------------------#
 
-# Will be done in Bot
-
-# @app.route('/authorize')
-# def authorize(request):
-#     uri = aiogoogle.oauth2.authorization_url(
-#         client_creds=CLIENT_CREDS, state=state, access_type='offline', include_granted_scopes=True, login_hint=EMAIL, prompt='select_account'
-#     )
-#     # Step A
-#     return response.redirect(uri)
-
-#----------------------------------------------#
-#                                              #
-# **Step B (Check OAuth2 figure above)**       #
-#                                              #
-#----------------------------------------------#
-# NOTE:                                        #
-#  you should now be authorizing your app @    #
-#   https://accounts.google.com/o/oauth2/      #
-#----------------------------------------------#
-
-#----------------------------------------------#
-#                                              #
-# **Step C, D & E (Check OAuth2 figure above)**#
-#                                              #
-#----------------------------------------------#
-
-# Step C
-# Google should redirect current_user to
-# this endpoint with a grant code
 @app.route('/callback/aiogoogle')
 async def callback(request):
     if request.args.get('error'):
@@ -63,30 +26,20 @@ async def callback(request):
         # TODO: uncomment and check states in DB to connect accout to chat
         # if returned_state != state:
         #     raise ServerError('NO')
-        # Step D & E (D send grant code, E receive token info)
-        full_user_creds = await gmpart_api.aiogoogle.oauth2.build_user_creds(
-            grant = request.args.get('code'),
-            client_creds = gmpart_api.CLIENT_CREDS
-        )
-        print(gmpart_api)
-        gmpart_api.init_entry_point(full_user_creds)
+        await gmpart_api.build_user_creds(request.args.get('code'))
         # TODO: delete link from chat
-        return response.json(full_user_creds)
+        print(await gmpart_api.messages_list())
+        print(gmpart_api.user_creds)
+        return response.json(gmpart_api.user_creds)
     else:
         # Should either receive a code or an error
         return response.text("Something's probably wrong with your callback")
 
 async def messages_list():
-    async with Aiogoogle(client_creds = TEST_CLIENT_CREDS, user_creds = test_user_creds) as gmpart:
-        gmpart_api = await gmpart.discover('gmail', 'v1')
-        messages = await gmpart.as_user(
-            gmpart_api.users.messages.list(
-                userId='me', 
-                labelIds='INBOX', 
-                includeSpamTrash=True, 
-                maxResults=5)
-            )
-    print(messages)
+    gm_api = Gmpart(TEST_CLIENT_CREDS, test_user_creds)
+    print(await gm_api.messages_list())
+
+
 if __name__ == '__main__':
     # gmpart_api = Gmpart(TEST_CLIENT_CREDS)
     # print(gmpart_api.authorize_uri('TEST_EMAIL'))
