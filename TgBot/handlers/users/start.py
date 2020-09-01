5556ba858b1232aa2779cfadfb3884a769ac693c
Moxxiq <dimanavsisto@gmail.com>
2020-09-01 14:00:51 +0300

    Formatting in greeting

--- TgBot/handlers/users/start.py
+++ TgBot/handlers/users/start.py
@@ -7,6 +7,6 @@ from TgBot.loader import dp
 @dp.message_handler(CommandStart())
 async def bot_start(message: types.Message):
     await message.answer(
-    	f'Привіт, {message.from_user.full_name}!'
+    	f'Привіт, {message.from_user.full_name}! '
     	'Я можу пересилати повідомлення з GMail у цей чат. '
     	'Щоб додати пошту натисніть /add та введіть пошту')
