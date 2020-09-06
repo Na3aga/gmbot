c89c39f6202cab68bea270dfc462c32eaf3573be
Moxxiq <dimanavsisto@gmail.com>
2020-09-07 00:01:39 +0300

    DB create type fix

--- DB/PostgreSQL.py
+++ DB/PostgreSQL.py
@@ -21,7 +21,7 @@ class DataBase():
     @conn
     async def create_db(self, conn):
         logging.info('Creating DB ...')
-        with open("create_db.sql", "r") as file:
+        with open("create.sql", "r") as file:
             create_db_command = file.read()
         await conn.execute(create_db_command)
 
