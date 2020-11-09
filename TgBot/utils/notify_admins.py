061bd0518ad442a640ee8e29382272dc9c1ef6de
Moxxiq <dimanavsisto@gmail.com>
2020-11-09 11:43:01 +0200

    Make admin notification silent

--- TgBot/utils/notify_admins.py
+++ TgBot/utils/notify_admins.py
@@ -8,7 +8,7 @@ from config import admins
 async def admins_notify(dp: Dispatcher, text="Я працюю!"):
     for admin in admins:
         try:
-            await dp.bot.send_message(admin, text)
+            await dp.bot.send_message(admin, text, disable_notification=True)
 
         except Exception as err:
             logging.exception(err)
