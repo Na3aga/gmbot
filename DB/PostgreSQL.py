17d591ed508e7dc9a270228ff38d7fc08e7b2771
Moxxiq <dimanavsisto@gmail.com>
2021-01-21 21:44:34 +0200

    A few important functions
    email_in_chat()
    add_watched_chat_emails()
    email_watched()
    add_watched_email()
    
    PEP8 changes
    Arguments docs
    Type annotations

--- DB/PostgreSQL.py
+++ DB/PostgreSQL.py
@@ -7,11 +7,13 @@ def conn(func):
     """
     Add every connection to a connection pool
     """
+
     async def decor(self, *args, **kwargs):
         async with self.pool.acquire() as conn:
             # TODO: make working transaction wrapper (? in separate func)
             # try `return await func(self, conn, *args, **kwargs)` to be able to use *args
             return await func(self, conn=conn, *args, **kwargs)
+
     return decor
 
 
@@ -34,6 +36,9 @@ class DataBase:
     async def create_db(self, conn):
         """
         Create DB from specified file
+
+        Args:
+            conn (asyncpg.pool.PoolAcquireContext): use decorator's connection pool
         """
         logging.info('Creating DB ...')
         with open("DB/create.sql", "r") as file:
@@ -43,10 +48,11 @@ class DataBase:
     @conn
     async def add_chat(self, conn, chat_id, chat_type):
         """
-        Add chat to DB
-        Parameters:
-        chat_id (int): chat id can be up to 52 bits
-        chat_type (str of enum): can be 'private', 'group', 'supergroup', 'channel'
+        Add a chat to DB
+        Args:
+            conn (asyncpg.pool.PoolAcquireContext): same as in create_db
+            chat_id (int): chat id can be up to 52 bits
+            chat_type (str of enum): can be 'private', 'group', 'supergroup', 'channel'
         """
         logging.debug(f"Adding chat {chat_id}")
         await conn.execute(
@@ -57,22 +63,17 @@ class DataBase:
             chat_type)
 
     @conn
-    async def add_gmail(self,
-                        conn,
-                        email,
-                        refresh_token,
-                        access_token,
-                        expires_at,
-                        chat_id):
+    async def add_gmail(self, conn, email, refresh_token, access_token,
+                        expires_at, chat_id):
         """
         Calling stored procedure in DB that adds email and its data and link
         FK with each other
-        Parameters:
-        email (str): gmail address up to 128 bytes
-        refresh_token (str): refresh token for offline access up to 512 bytes
-        access_token (str): access user token  up to 2048 bytes
-        expires_at (str): time of token expiration to 32 bytes
-        chat_id (int): gmail address up to 128 bytes
+        Args:
+            email (str): gmail address up to 128 bytes
+            refresh_token (str): refresh token for offline access up to 512 bytes
+            access_token (str): access user token  up to 2048 bytes
+            expires_at (str): time of token expiration to 32 bytes
+            chat_id (int): chat id can be up to 52 bits
         """
         logging.debug(f"Gmail {email} is adding...")
         await conn.execute(
@@ -84,13 +85,13 @@ class DataBase:
             chat_id)
 
     @conn
-    async def get_gmail_creds(self, conn, email):
+    async def get_gmail_creds(self, conn, email) -> asyncpg.Record:
         """
         Get user credentials with that email
-        Parameters:
-        email (str): gmail address up to 128 bytes
+        Args:
+            email (str): gmail address up to 128 bytes
         Returns:
-        (asyncpg.Record): row with user credentials
+            (asyncpg.Record): row with user credentials
         """
         logging.debug(f"Getting credentials from {email}")
         return await conn.fetchrow(
@@ -98,3 +99,70 @@ class DataBase:
             from gmail
             where email = $1""",
             email)
+
+    @conn
+    async def email_in_chat(self, conn, email, chat_id) -> asyncpg.Record:
+        """
+        Get email, chat if they are linked in 'chat_gmail' table
+        Args:
+            conn: (asyncpg.pool.PoolAcquireContext): same as in create_db
+            email (str): gmail address up to 128 bytes
+            chat_id (int): chat id can be up to 52 bits
+
+        Returns:
+            (asyncpg.Record): row with email and chat ID if they are exists in the table
+        """
+        logging.debug(f"Checking if chat '{chat_id}' has email '{email}'")
+        return await conn.fetchrow(
+            """select email, chat_id
+            from chat_gmail
+            where email = $1 and chat_id=$2""",
+            email, chat_id)
+
+    @conn
+    async def add_watched_chat_emails(self, conn, email, chat_id):
+        """
+        Adding already linked chat and email from 'chat_gmail' table to watched_chat_emails
+        Args:
+            same as in email_in_chat()
+        """
+        logging.debug(f"Add existing chat_email to watched {email}, {chat_id}")
+        await conn.execute(
+            """insert into watched_chat_emails (email, chat_id)
+            select email, chat_id
+            from chat_gmail
+            where email = $1 and chat_id = $2""",
+            email, chat_id)
+
+    @conn
+    async def email_watched(self, conn, email) -> asyncpg.Record:
+        """
+        Get email if it 'watched_emails' table
+        Args:
+            conn, email: same as in email_in_chat()
+
+        Returns:
+            same as in email_in_chat()
+        """
+        logging.debug(f"Checking if email '{email}' is watched")
+        return await conn.fetchrow(
+            """select email
+            from watched_emails
+            where email = $1""",
+            email
+        )
+
+    @conn
+    async def add_watched_email(self, conn, email) -> asyncpg.Record:
+        """
+        Adding already existing email from 'gmail' table to 'watched_emails' with timestamp
+        Args:
+            same as in email_watched()
+        """
+        logging.debug(f"Start watching email '{email}'")
+        await conn.execute(
+            """insert into watched_emails (email, last_watch)
+            values ((select email from gmail where gmail.email = $1), now())
+            """,
+            email
+        )
